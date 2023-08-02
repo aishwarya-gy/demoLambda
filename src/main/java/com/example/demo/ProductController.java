72ae51d42a9bbb3363ba40de98fb3b1ac3e138c0
aishwarya-gy <aishwarya_rohatgi@goodyear.com>
2023-08-02 10:04:52 +0530

    first commit

---
+++ src/main/java/com/example/demo/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product savedProduct = this.productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Optional<Product>> getProduct(@PathVariable Long id){
+        Optional<Product> product = this.productService.getProduct(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/demo/ProductService.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product saveProduct(Product product){
+        return this.productRepository.save(product);
+    }
+
+    public Optional<Product> getProduct(Long productId){
+        return this.productRepository.findById(productId);
+    }
+}
